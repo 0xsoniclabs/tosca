dab1bb4e54bc2d413f2bc0e90cc0562568a39c45
Lorenz Schüler <lorenz@saarow.de>
2025-03-07 16:06:44 +0100

    improve plot_benchstat.py

--- rust/scripts/plot_benchstat.py
+++ rust/scripts/plot_benchstat.py
@@ -1,55 +1,31 @@
-# USAGE: benchstat ... | python3 plot_benchstat.py
-# to export to other formats just adjust the file_format variable
+# USAGE: benchstat -format scv ... | python3 plot_benchstat.py <benchmark>
+# 
+# To export to other formats just adjust the file_format variable.
 #
 # REQUIREMENTS:
-# sys and re usually come with the python installation but matplotlib as to be installed manually
-# pip3 install matplotlib
-#
+# pip3 install matplotlib csv
 
 import sys
 import matplotlib.pyplot as plt
 import matplotlib
-import re
+import csv
 
 file_format = "png" # "svg"
 
-def parse_benchstat_output(benchstat_output):
-    lines = benchstat_output.strip().splitlines()
-    lines = lines[4:] # skip config lines
+def parse_csv(csv_input):
+    reader = list(csv.reader(csv_input.strip().split("\n")[4:]))
     
-    version_headers = [col.strip() for col in lines[0].split("│")[1:-1]]
-    lines = lines[2:]
-    
-    # match number followed by n (nano), µ (micro), m (milli), or s (seconds)
-    time_pattern = r'([\d\.]+)(n|µ|m|s)'
+    interpreters = reader[0][1:]
     
     data = {}
-    for line in lines:
-        # split at at least 2 whitespace chars
-        parts = re.split(r'\s{2,}', line.strip())
-        
-        benchmark_name = parts[0]
-        
+    for (j, row) in enumerate(reader[2:]):
+        benchmark = row[0]
         benchmark_data = {}
-        i = 0
-        for result in parts[1:]:
-            match = re.search(time_pattern, result)
-            if match:
-                time_value = float(match.group(1))
-                time_unit = match.group(2)
-
-                if time_unit == 'µ':
-                    time_value *= 1e3
-                elif time_unit == 'm':
-                    time_value *= 1e6
-                elif time_unit == 's':
-                    time_value *= 1e9
-
-                interpreter = version_headers[i] if i < len(version_headers) else f"Version {i + 1}"
-                
-                benchmark_data[interpreter] = time_value
-                i = i + 1
-        data[benchmark_name] = benchmark_data
+        for (i, cell) in enumerate(row[1:]):
+            if i == 0 or (i-2) % 4 == 0: # time column
+                interpreter = interpreters[i]
+                benchmark_data[interpreter] = float(cell)
+        data[benchmark] = benchmark_data
     
     return data
 
@@ -57,55 +33,59 @@ def generate_colors(num_colors):
     color_map = matplotlib.colormaps["tab20"]
     return [color_map(i / num_colors) for i in range(num_colors)]
 
-def plot_benchmarks(data):
-    benchmarks = list(data.keys())
-    interpreters = list(data[benchmarks[0]].keys())
+def plot_benchmark(data):
+    interpreters, times = list(zip(*data.items()))
 
     colors = generate_colors(len(interpreters))
 
-    for benchmark in benchmarks:
-        fig, ax = plt.subplots(figsize=(10, 15))
+    fig, ax = plt.subplots(figsize=(10, 6))
+    bars = plt.bar(interpreters, times, color=colors[:len(interpreters)])
+    for (bar, interpreter, time) in zip(bars, interpreters, times):
+        height = ax.get_ylim()[1]
+        if time > 1:
+            label = f'{time:.3f}s'
+        elif time > 1e-3:
+            label = f'{time/1e-3:.3f}ms'
+        elif time > 1e-6:
+            label = f'{time/1e-6:.3f}µs'
+        else:
+            label = f'{time/1e-9:.3f}ns'
+        ax.text(
+            bar.get_x() + bar.get_width() / 2,
+            time + height * 0.01,
+            label,
+            ha='center',
+            va='bottom',
+            fontsize=10,
+        )
+        ax.text(
+            bar.get_x() + bar.get_width() / 2,
+            height * 0.01,
+            interpreter,
+            ha='center',
+            va='bottom',
+            fontsize=10,
+            rotation=90
+        )
 
-        times = [data[benchmark][interpreter] for interpreter in interpreters]
+    plt.xlabel('Interpreter')
+    plt.ylabel('Time in Seconds / Benchmark Run')
+    plt.title(benchmark_name)
 
-        bars = ax.bar(interpreters, times, color=colors[:len(interpreters)])
-        for (bar, time) in zip(bars, times):
-            height = ax.get_ylim()[1]
-            if time > 1e9:
-                label = f'{time/1e9:.3f}s'
-            elif time > 1e6:
-                label = f'{time/1e6:.3f}ms'
-            elif time > 1e3:
-                label = f'{time/1e3:.3f}µs'
-            else:
-                label = f'{time:.3f}ns'
-            ax.text(
-                bar.get_x() + bar.get_width() / 2,
-                time + height * 0.01,
-                label,
-                ha='center',
-                va='bottom',
-                fontsize=10,
-                rotation=90
-            )
-        handles = [plt.Line2D([0], [0], color=color, lw=4) for color in colors]
-        ax.legend(handles, interpreters, title='Interpreter', loc='upper center', bbox_to_anchor=(0.5, -0.05))
+    plt.xticks([])
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
 
-        ax.set_xlabel('Interpreter')
-        ax.set_ylabel('Time (ns/op)')
-        ax.set_title(benchmark)
+args = sys.argv
+if len(args) != 2:
+    print("USAGE: benchstat -format scv ... | python3 plot_benchstat.py <benchmark>")
+    sys.exit()
 
-        ax.grid(True)
-
-        plt.xticks([])
-        plt.tight_layout()
-        plt.savefig(f'{benchmark.replace("/", "_")}.{file_format}')
-        plt.close(fig)
-
-def main():
-    benchstat_output = sys.stdin.read()
-    parsed_data = parse_benchstat_output(benchstat_output)
-    plot_benchmarks(parsed_data)
-
-if __name__ == "__main__":
-    main()
+benchmark_name = args[1]
+benchstat_output = sys.stdin.read()
+parsed_data = parse_csv(benchstat_output)
+if benchmark_name in parsed_data:
+    plot_benchmark(parsed_data[benchmark_name])
+else:
+    print(f"invalid benchmark name: {benchmark_name}")
