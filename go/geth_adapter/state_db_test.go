17df00b106f63d4bf66e7475b1d1074614e937d1
simonlechner <73333654+simonlechner@users.noreply.github.com>
2025-09-05 08:22:39 +0200

    Update geth dependency to 1.16.2 (#131)

--- go/geth_adapter/state_db_test.go
+++ go/geth_adapter/state_db_test.go
@@ -13,9 +13,31 @@ package geth_adapter
 import (
 	"testing"
 
+	"github.com/0xsoniclabs/tosca/go/tosca"
+	common "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
 func TestStateDB_implementsVmStateDBInterface(t *testing.T) {
 	var _ vm.StateDB = &StateDB{}
+}
+
+func TestStateDB_GetStateAndCommittedStateReturnsOriginalAndCurrentState(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	context := tosca.NewMockTransactionContext(ctrl)
+
+	address := tosca.Address{0x1}
+	key := tosca.Key{0x2}
+	original := tosca.Word{0x3}
+	current := tosca.Word{0x4}
+
+	context.EXPECT().GetStorage(address, key).Return(original)
+	context.EXPECT().GetCommittedStorage(address, key).Return(current)
+	stateDB := NewStateDB(context)
+
+	state, committed := stateDB.GetStateAndCommittedState(common.Address(address), common.Hash(key))
+	require.Equal(t, original, tosca.Word(state))
+	require.Equal(t, current, tosca.Word(committed))
 }
