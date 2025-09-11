2bfb2e5902cab2e3bfbfa325154f9d1dd5ef78f3
Simon <simon@fantom.foundation>
2025-09-11 12:10:39 +0200

    Refactor test

--- go/ct/common/u256_test.go
+++ go/ct/common/u256_test.go
@@ -255,20 +255,10 @@ func TestU256Not(t *testing.T) {
 }
 
 func TestU256BitLen(t *testing.T) {
-	if NewU256(0).BitLen() != 0 {
-		t.Fail()
-	}
-	if NewU256(1).BitLen() != 1 {
-		t.Fail()
-	}
-	if NewU256(2).BitLen() != 2 {
-		t.Fail()
-	}
-	if NewU256(4).BitLen() != 3 {
-		t.Fail()
-	}
-	if NewU256(0).Not().BitLen() != 256 {
-		t.Fail()
+	for i := range 256 {
+		if NewU256(1).Shl(NewU256(uint64(i))).BitLen() != i+1 {
+			t.Fail()
+		}
 	}
 }
 
