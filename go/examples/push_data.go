bec467fe471def3f837a5e2b8118974ab0e89a8c
simonlechner <73333654+simonlechner@users.noreply.github.com>
2025-11-13 16:52:56 +0100

    Add push data benchmark (#150)

---
+++ go/examples/push_data.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2025 Sonic Operations Ltd
+//
+// Use of this software is governed by the Business Source License included
+// in the LICENSE file and at soniclabs.com/bsl11.
+//
+// Change Date: 2028-4-16
+//
+// On the date above, in accordance with the Business Source License, use of
+// this software will be governed by the GNU Lesser General Public License v3.
+
+package examples
+
+import "github.com/0xsoniclabs/tosca/go/tosca/vm"
+
+func GetPushDataExample() Example {
+	iterations := 100
+	code := make([]byte, 0, iterations*34+5)
+	for range iterations {
+		code = append(code, []byte{
+			byte(vm.PUSH32),
+			byte(0), byte(1), byte(2), byte(3), byte(4), byte(5), byte(6), byte(7), byte(8),
+			byte(9), byte(10), byte(11), byte(12), byte(13), byte(14), byte(15), byte(16),
+			byte(17), byte(18), byte(19), byte(20), byte(21), byte(22), byte(23), byte(24),
+			byte(25), byte(26), byte(27), byte(28), byte(29), byte(30), byte(31),
+			byte(vm.POP),
+		}...)
+	}
+	code = append(code, []byte{
+		byte(vm.PUSH1), 32,
+		byte(vm.PUSH1), 0,
+		byte(vm.RETURN),
+	}...)
+
+	return exampleSpec{
+		Name:      "pushData",
+		Code:      code,
+		reference: pushData,
+	}.build()
+}
+
+func pushData(_ int) int {
+	return 0
+}
